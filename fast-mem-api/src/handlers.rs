6f9e21e4b95c72572b7990291b9419c8299e3d62
Eduardo Díaz <ediaz@dcc8090.cl>
2020-07-26 16:07:56 -0400

    add count

--- fast-mem-api/src/handlers.rs
+++ fast-mem-api/src/handlers.rs
@@ -1,22 +1,44 @@
-use actix_web::{Error, web, Responder, HttpRequest, HttpResponse};
+use actix_web::{web, Error, HttpRequest, HttpResponse, Responder};
+use csv::ReaderBuilder;
 use futures::future::{ready, Ready};
 use rand::{thread_rng, Rng};
-use serde::{Serialize, Deserialize};
-use csv::ReaderBuilder;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::time::Instant;
 use std::sync::Mutex;
+use std::time::Instant;
 
-
-
-pub struct AppState {
-    app_data: Mutex<HashMap<usize, u8>>,
+#[derive(Clone, Serialize)]
+pub struct AppData {
+    value: u8,
+    count: u64,
 }
 
+pub struct AppState {
+    app_data: Mutex<HashMap<usize, AppData>>,
+}
+
+impl AppState {
+
+    fn len(&self) -> usize {
+       self.app_data.lock().unwrap().len()
+    }
+
+    fn access(&self, id: &usize) -> Option<AppData> {
+        {
+            let mut app_data = self.app_data.lock().unwrap();
+            if let Some(data) = app_data.get_mut(&id) {
+                data.count += 1;
+                return Some((*data).clone());
+            }
+        }
+       self.app_data.lock().unwrap().get(id).cloned()
+    }
+
+}
 #[derive(Serialize)]
 pub struct ResponseObj {
     id: usize,
-    value: Option<u8>,
+    data: Option<AppData>,
 }
 
 impl Responder for ResponseObj {
@@ -25,25 +47,23 @@ impl Responder for ResponseObj {
 
     fn respond_to(self, _req: &HttpRequest) -> Self::Future {
         let body = serde_json::to_string(&self).unwrap();
-        match self.value {
+        match self.data {
             Some(_) => ready(Ok(HttpResponse::Ok()
                 .content_type("application/json")
                 .body(body))),
             None => ready(Ok(HttpResponse::NotFound()
                 .content_type("application/json")
-                .body(body)))
+                .body(body))),
         }
         // Create response and set content type
-
     }
-
 }
 
 pub async fn index(data: web::Data<AppState>) -> impl Responder {
-    let id : usize = thread_rng().gen_range(0, data.app_data.lock().unwrap().len());
+    let id: usize = thread_rng().gen_range(0, data.len());
     ResponseObj {
         id,
-        value: data.app_data.lock().unwrap().get(&id).cloned(),
+        data: data.access(&id),
     }
 }
 
@@ -52,14 +72,18 @@ pub struct ApiParams {
     id: usize,
 }
 
-
 pub async fn api(param: web::Path<ApiParams>, data: web::Data<AppState>) -> impl Responder {
-        ResponseObj {
-            id: param.id,
-            value: data.app_data.lock().unwrap().get(&param.id).cloned()
-        }
+    ResponseObj {
+        id: param.id,
+        data: data.access(&param.id),
+    }
 }
 
+/*
+pub async fn stat(data: web::Data<AppState>) -> Responder {
+
+}
+*/
 #[derive(Debug, Deserialize)]
 struct Record {
     id: usize,
@@ -70,18 +94,23 @@ impl AppState {
     pub fn load_data(_filename: &str) -> AppState {
         let start = Instant::now();
         println!("loading data from {}", _filename);
-        let mut data =  HashMap::new();
+        let mut data = HashMap::new();
 
         let mut reader = ReaderBuilder::new().from_path(_filename).unwrap();
         for row in reader.deserialize() {
-
-            let record : Record = row.unwrap();
-            data.insert(record.id, record.value);
+            let record: Record = row.unwrap();
+            data.insert(
+                record.id,
+                AppData {
+                    value: record.value,
+                    count: 0,
+                },
+            );
         }
         let end = Instant::now();
         println!("data loaded in {:?}", end.duration_since(start));
         AppState {
-            app_data: Mutex::new(data)
+            app_data: Mutex::new(data),
         }
     }
 }
--- fast-mem-api/src/main.rs
+++ fast-mem-api/src/main.rs
@@ -4,14 +4,14 @@ mod handlers;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let web_data = web::Data::new( handlers::AppState::load_data("../data.csv"));
+    let web_data = web::Data::new(handlers::AppState::load_data("../data.csv"));
     HttpServer::new(move || {
         App::new()
             .app_data(web_data.clone())
             .route("/", web::get().to(handlers::index))
             .route("/api/{id}", web::get().to(handlers::api))
     })
-        .bind("127.0.0.1:8088")?
-        .run()
-        .await
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
